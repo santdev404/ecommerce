a75905ebe14286dbed1230dbc1dc6db7a365bfcc
Santdev404 <santdev404@gmail.com>
2021-06-17 11:07:38 -0500

    Editing categories & subcategories inside navigation-subcategories component

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,16 @@
-<header class="bg-trueGray-700">
+<style>
+    #navigation-menu{
+        height: calc(100vh - 4rem);
+    }
+
+    .navigation-link:hover .navigation-submenu{
+        display: block !important;
+    }
+
+    
+</style>
+
+<header class="bg-trueGray-700 sticky top-0">
     <div class="container flex items-center h-16">
         <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
 
@@ -87,4 +99,37 @@
         @livewire('dropdown-cart')
 
     </div>
+
+    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+        <div class="container h-full">
+            <div class="grid grid-cols-4 h-full relative">
+                <ul class="bg-white">
+                    @foreach ($categories as $category)
+                        <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                                <span class="flex justify-center w-9">
+                                    {!!$category->icon!!}
+                                </span>
+                                {{$category->name}}
+                            </a>
+
+                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+  
+                                <x-navigation-subcategories :category="$category"/>
+
+                            </div>
+
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="col-span-3 bg-gray-100">
+
+                    <x-navigation-subcategories :category="$categories->first()"/>
+                   
+
+                </div>
+            </div>
+        </div>
+    </nav>
+
 </header>
