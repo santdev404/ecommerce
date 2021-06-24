804953580dec6cc6be9876969e3728606ccb88aa
Santdev404 <santdev404@gmail.com>
2021-06-24 15:57:37 -0500

    Catgory 2

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 use Livewire\WithPagination;
+
+
 
 class CategoryFilter extends Component
 {
@@ -13,9 +17,53 @@ class CategoryFilter extends Component
 
     public $category;
 
+    public $subcategoria;
+
+    public $marca;
+
+    public function limpiar(){
+        $this->reset(['subcategoria','marca']);
+    }
+
     public function render()
     {
-        $products = $this->category->products()->where('status',2)->paginate(20);
+        // $products = $this->category->products()->where('status',2)->paginate(20);
+
+
+        //Consulta
+        $productsQuery = Product::query()->whereHas('subcategory.category', function(Builder $query){
+            $query->where('id', $this->category->id);
+        });
+
+        
+
+        if($this->subcategoria){
+
+            $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
+                $query->where('name', $this->subcategoria);
+            });
+
+            
+        }
+
+
+        if($this->marca){
+
+            $productsQuery = $productsQuery->whereHas('brand', function(Builder $query){
+                $query->where('name', $this->marca);
+            });
+
+            
+        }
+
+        
+
+        //Coleccion
+        $products = $productsQuery->paginate(20);
+
+        // echo getType($products).PHP_EOL;
+        // var_dump($products).PHP_EOL;
+        // die();
 
         return view('livewire.category-filter',compact('products'));
     }
